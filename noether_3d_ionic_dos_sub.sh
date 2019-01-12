638be4ac7a6926657ec87dba3aac7fcf9f5e67aa
gour-jana-noether <gour.jana@niser.ac.in>
2019-01-12 19:02:10 +0530

    mtfort.f_discurd

--- noether_3d_ionic_dos_sub.sh
+++ noether_3d_ionic_dos_sub.sh
@@ -12,7 +12,7 @@ do
 mkdir -p U_$U1
 cd U_$U1
 
-for tp in 0.0 
+for tp in 0.4 
 do 
 mkdir -p tp_$tp
 cd tp_$tp
@@ -22,14 +22,14 @@ do
 mkdir -p seed_$seed
 cd seed_$seed
 
-for strnth in  0.5 
+for strnth in  0.0 
 do
 mkdir -p strnth_$strnth
 cd strnth_$strnth
 
 
 cp ../../../../../3d_ionic_dos_and_energy.f90 .
-cp ../../../../../main_data/U_$U1/tp_$tp/seed_$seed/strnth_$strnth/main_input.dat .
+cp ../../../../../U_$U1/tp_$tp/seed_$seed/strnth_$strnth/main_input.dat .
 
 
 #!******************************************************************************
@@ -38,14 +38,14 @@ echo   "4                          !dc "            >> $datafile
 echo   "-1.0d0                     !t1     "        >> $datafile
 echo   $tp"0d0                      !t2     "        >> $datafile
 echo   "29                         !Tgrid_max "      >> $datafile
-echo   "800                       !MCSW "          >> $datafile
+echo   "2000                       !MCSW "          >> $datafile
 echo   "10                          !intrvl   "      >> $datafile
 echo   $U1"d0                      !U1 "             >> $datafile  
 echo   "1.0d0                      !filling "       >> $datafile
 echo   "0.02                       !gama_m "          >> $datafile 
 echo   $strnth"0d0                       !strnth "        >> $datafile 
 #!******************************************************************************
-echo "../../../../../main_data/U_$U1/tp_$tp/seed_$seed/strnth_$strnth"    >> $pathfile
+echo "../../../../../U_$U1/tp_$tp/seed_$seed/strnth_$strnth"    >> $pathfile
 #!******************************************************************************
 
 gfortran -o run.x   3d_ionic_dos_and_energy.f90  -llapack -lblas
@@ -55,8 +55,7 @@ echo "#PBS  -N   3DIdos"$U1"_"$tp"_"$strnth                            > $sub
 echo "#PBS -l nodes=1:ppn=1"                        >>$sub
 echo "#PBS -j oe "                                  >> $sub   
 echo "#PBS -o out.log"                                  >> $sub 
-echo "#PBS -e err.log"                                  >> $sub 
-echo  "#PBS -q serial      	"	               >> $sub                    
+echo "#PBS -e err.log"                                  >> $sub              
 echo "cd"    \$PBS_O_WORKDIR                         >> $sub
 echo "date"                                         >> $sub
 echo "./run.x <dos_input.dat> dos_out "                    >> $sub
